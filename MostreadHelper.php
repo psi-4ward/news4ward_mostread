b99f7fd18201674802329a6b7207e50276ee6173
Christoph Wiechert <wio@psitrax.de>
2014-04-30 20:29:26 +0200

    Use new News4wardParseArticle Hook

--- MostreadHelper.php
+++ MostreadHelper.php
@@ -10,14 +10,12 @@ namespace Psi\News4ward;
 class MostreadHelper
 {
 
-    public function mostreadParseArticle($obj, $arrArticles, $objTemplate)
+    public function mostreadParseArticle($obj, $article, $objTemplate)
     {
         // Use the aliased class!
         if(! $obj instanceof \News4ward\Module\Reader) return;
 
-        foreach($arrArticles as $article) {
-            \Database::getInstance()->prepare('UPDATE tl_news4ward_article SET hits = hits + 1 WHERE id=?')->execute($article['id']);
-        }
+        \Database::getInstance()->prepare('UPDATE tl_news4ward_article SET hits = hits + 1 WHERE id=?')->execute($article['id']);
     }
 
 }
